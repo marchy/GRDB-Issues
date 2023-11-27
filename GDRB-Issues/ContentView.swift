08feffb95ab1649a555104c777599351983645a6
Marcel Bradea <mbradea@gmail.com>
2023-11-27 17:13:06 -0500

    Barebones project

---
+++ GDRB-Issues/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  GDRB-Issues
+//
+//  Created by Marcel Bradea on 2023-11-27.
+//
+
+import SwiftUI
+
+
+struct ContentView : View {
+	var body:some View {
+		VStack {
+			Image(systemName: "globe")
+				.imageScale(.large)
+				.foregroundStyle(.tint)
+			Text("Hello, world!")
+		}.padding()
+	}
+}
+
+
+#Preview {
+	ContentView()
+}
---
+++ GDRB-Issues/GDRBIssuesApp.swift
@@ -0,0 +1,18 @@
+//
+//  GDRB_IssuesApp.swift
+//  GDRB-Issues
+//
+//  Created by Marcel Bradea on 2023-11-27.
+//
+
+import SwiftUI
+
+
+@main
+struct GDRBIssuesApp : App {
+	var body:some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
